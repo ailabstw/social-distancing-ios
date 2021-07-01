8dbbb3fb4506b68f37ecc3fc0a3f68e2f3d8a681
Shiva Huang <shiva.huang@ailabs.tw>
2021-07-01 10:16:29 +0800

    Modify Localizations.swift
    Add default value to localizations.

--- ExposureNotification/Models/Localizations.swift
+++ ExposureNotification/Models/Localizations.swift
@@ -14,27 +14,37 @@ extension Localizations {
     enum Alert {
         enum Button {
             static let allow = NSLocalizedString("Alert.Button.Allow",
+                                                 value: "Allow",
                                                  comment: "The button title on alert to allow app to grant permission")
             static let cancel = NSLocalizedString("Alert.Button.Cancel",
+                                                  value: "Cancel",
                                                   comment: "The button title on alert to cancel action")
             static let notAllow = NSLocalizedString("Alert.Button.NotAllow",
+                                                    value: "Don't Allow",
                                                     comment: "The button title on alert to not allow app to grant permission")
             static let later = NSLocalizedString("Alert.Button.Later",
+                                                 value: "Later",
                                                  comment: "The button title on alert for doing something later")
             static let no = NSLocalizedString("Alert.Button.No",
+                                              value: "No",
                                               comment: "The button title on alert for No")
             static let ok = NSLocalizedString("Alert.Button.OK",
+                                              value: "OK",
                                               comment: "The button title on alert for OK")
             static let submit = NSLocalizedString("Alert.Button.Submit",
+                                                  value: "Submit",
                                                   comment: "The button title on alert to submit data")
             static let yes = NSLocalizedString("Alert.Button.Yes",
+                                               value: "Yes",
                                                comment: "The button title on alert for Yes")
         }
 
         enum Message {
             static let uploadFailed = NSLocalizedString("Alert.Message.UploadFailed",
+                                                        value: "Upload Failed",
                                                         comment: "The message on alert to indicate the upload is failed")
             static let uploadSucceed = NSLocalizedString("Alert.Message.UploadSucceed",
+                                                         value: "Upload Successful",
                                                          comment: "The message on alert to indicate the upload is succeed")
         }
     }
@@ -43,20 +53,25 @@ extension Localizations {
 extension Localizations {
     enum EnableBluetoothAlert {
         static let title = NSLocalizedString("EnableBluetoothAlert.Title",
+                                             value: "Activate your Bluetooth",
                                              comment: "The title on enabling bluetooth alert")
         static let message = NSLocalizedString("EnableBluetoothAlert.Message",
+                                               value: "Your device and the devices around you will exchange anonymous IDs via Bluetooth.",
                                                comment: "The message body on enabling bluetooth alert")
 
         enum Button {
             static let enable = NSLocalizedString("EnableBluetoothAlert.Button.Enable",
+                                                  value: "OK",
                                                   comment: "The button title on enabling bluetooth alert to enable bluetooth")
         }
     }
 
     enum NotificationNotEnabledAlert {
         static let title = NSLocalizedString("NotificationNotEnabledAlert.Title",
+                                             value: "Exposure Notification is not enabled",
                                              comment: "The title on notification not enabled alert")
         static let message = NSLocalizedString("NotificationNotEnabledAlert.Message",
+                                               value: "Enable Exposure Notification for possible contact with COVID-Positive persons.",
                                                comment: "The body message on notification not enabled alert")
     }
 }
@@ -64,18 +79,23 @@ extension Localizations {
 extension Localizations {
     enum BluetoothNotEnabledNotification {
         static let title = NSLocalizedString("BluetoothNotEnabledNotification.Title",
+                                             value: "Exposure Notification Disabled",
                                              comment: "The title on bluetooth not enabled notification")
         static let message = NSLocalizedString("BluetoothNotEnabledNotification.Message",
+                                               value: "Exposure Notification disabled due to Bluetooth is deactivated",
                                                comment: "The body message on bluetooth not enabled notification")
     }
 
     enum DetectionResultNotification {
         static let title = NSLocalizedString("DetectionResultNotification.Title",
+                                             value: "Contact Tracing Results",
                                              comment: "The title on detection result notification")
         enum Message {
             static let clear = NSLocalizedString("DetectionResultNotification.Message.Clear",
+                                                 value: "No exposure detected",
                                                  comment: "The body message on detection result notification for not risky")
             static let risky = NSLocalizedString("DetectionResultNotification.Message.Risky",
+                                                 value: "You may be at risk for exposure to COVID-19, open the app for more details",
                                                  comment: "The body message on detection result notification for risky")
         }
     }
